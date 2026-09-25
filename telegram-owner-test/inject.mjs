9cb2fcedc4037b95d4539b38385e34f62563b5e8
Jahongir5858 <62850042+Jahongir5858@users.noreply.github.com>
2026-09-25 19:43:55 +0500

    Add Telegram Owner Presence test build

---
+++ telegram-owner-test/inject.mjs
@@ -0,0 +1,9 @@
+import fs from 'node:fs';
+const file=process.argv[2]||'index.html';
+let html=fs.readFileSync(file,'utf8');
+const tag='<script defer src="/owner-presence.js"></script>';
+if(!html.includes(tag)){
+  if(html.includes('</body>')) html=html.replace('</body>',tag+'\n</body>');
+  else html+=tag;
+}
+fs.writeFileSync(file,html);
